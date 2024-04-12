d14a871acc5d5825e677162c07eb9d1bc824002a
Sadia Khan <khansadia220@gmail.com>
2024-04-12 11:56:08 +0200

    create vite project and tailwind css

---
+++ Job-listing-website/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1 className='bg-blue-500 text-black-8xl'> Online Job Portal </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
